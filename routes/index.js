27e7607bd1357c5fb87ca49c6b65e1c56646797a
yh <yh@localhost.localdomain>
2023-02-03 17:56:13 +0100

    ldap search

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,38 @@ const qrcode = require('qrcode')
 
 const { spawnSync } = require('child_process');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'sign up' });
+const ldap = require('ldapjs');
+const lclient = ldap.createClient({
+    url: ['ldap://localhost:389']
 });
 
-router.post('/', function (req, res) {
-    console.log('Receiving params...');
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'sign up', err:'' });
+});
 
-    // out = spawnSync('ls', ['-l']).stdout;
-    let out = spawnSync('sh', ['setup.sh', `${req.body.emailaddress}`, `${req.body.password}`]);
+router.post('/', async function (req, res) {
+    console.log('Receiving params...');
     let msg = "";
+    
+    let rec = await checkUser(req.body.username);
+    console.log('ldap record: ' + rec);
+    if (rec) {
+        if (rec.userPassword != req.body.password) {
+            msg = 'Password is not correct!';
+            // res.send(msg);
+            res.render('index', { title:'sign up', err:msg });
+            return;
+        }
+    } else {
+        msg = `User is not found!`;
+        // res.send(msg);
+        res.render('index', { title:'sign up', err:msg });
+        return;
+    }
+    
+    // out = spawnSync('ls', ['-l']).stdout;
+    let out = spawnSync('sh', ['setup.sh', `${req.body.username}`, `${req.body.password}`]);
     if (out.error) {
         console.log(`error: ${error.message}`);
         msg += out.error.message;
@@ -40,41 +61,61 @@ router.post('/', function (req, res) {
             };
             qrcode.toDataURL(key, opts, (err, src) => {
                 if (err) res.send("Error occured");
-                res.send(`
-                    <!DOCTYPE html>
-                    <html>
-                    <head>
-                        <title>Sign up</title>
-                    </head>
-                    <body>
-                        <img class="mb-4" src="${src}" alt="" width="200" height="200"/>
-                        <p> ${lines[3].trim()} </p>
-                        <p> This is the original google QRcode <a href="${lines[2].trim()}" target="_blank">link</a>.</p>
-                        <a href="http://localhost:3000">Go Back</a>
-                    </body>
-                    </html>
-                    `);
+                // res.send(`
+                //     <!DOCTYPE html>
+                //     <html>
+                //     <head>
+                //         <title>Sign up</title>
+                //     </head>
+                //     <body>
+                //         <img class="mb-4" src="${src}" alt="" width="200" height="200"/>
+                //         <p> ${lines[3].trim()} </p>
+                //         <p> This is the original google QRcode <a href="${lines[2].trim()}" target="_blank">link</a>.</p>
+                //         <a href="http://localhost:3000">Go Back</a>
+                //     </body>
+                //     </html>
+                //     `);
+                res.render('info', {src:src, key:lines[3].trim(), link:lines[2].trim()});
             });
-
-            // msg = `
-            // <!DOCTYPE html>
-            // <html>
-            // <head>
-            //     <title>Sign up</title>
-            // </head>
-            // <body>
-            //     <img class="mb-4" src="${lines[3].trim().replace('www.google.com', 'chart.googleapis.com')}" alt="" width="200" height="200"/>
-            //     <p> ${lines[4].trim()} </p>
-            //     <p> If you can not see the QRcode, please copy this <a href="${lines[3].trim()}" target="_blank">link</a> and open it in a new window.</p>
-            //     <a href="http://localhost:3000">Go Back</a>
-            // </body>
-            // </html>
-            // `;
         }
-    }
-    else {
+    } else {
         res.send(msg);
     }
 });
+
+function checkUser(username) {
+    let lopts = {
+        filter: `(cn=${username})`,
+        scope: 'sub',
+        attributes: ['cn', 'userPassword'],
+        timeLimit: 5
+    };
+    let rec = new Promise((resolve, reject) => {
+        lclient.search('dc=test,dc=com', lopts, (err, ret) => {
+            let rec = null;
+            ret.on('searchRequest', (searchRequest) => {
+                console.log('searchRequest: ', searchRequest.messageID);
+            });
+            ret.on('searchEntry', (entry) => {
+                console.log('entry: ' + JSON.stringify(entry.object));
+                rec = entry.object;
+            });
+            ret.on('searchReference', (referral) => {
+                console.log('referral: ' + referral.uris.join());
+            });
+            ret.on('error', (err) => {
+                console.error('error: ' + err.message);
+                // lclient.unbind();
+                reject('error: ' + err.message);
+            });
+            ret.on('end', (result) => {
+                console.log('status: ' + result);
+                // lclient.unbind();
+                resolve(rec);
+            });
+        });
+    });
+    return rec;
+}
 
 module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,32 +1,79 @@
-const express = require('express')
-const qrcode = require('qrcode')
+const express = require('express');
+const qrcode = require('qrcode');
+const ldap = require('ldapjs');
 
 const { spawnSync } = require('child_process');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
-})
+});
 
-app.get('/', (req, res) => {
-    let opts = {
-        margin:10,
-        errorCorrectionLevel:'M',
-        width: 200
+const lclient = ldap.createClient({
+    url: ['ldap://localhost:389']
+});
+lclient.bind('cn=admin,dc=test,dc=com', '-pl,0okm', (err) => {
+    if(err) {
+        console.log('ldap binding failed.');
+    }
+});
+lclient.on('error', (err) => {
+    console.log(err);
+});
+
+app.get('/', async(req, res) => {
+    let lopts = {
+        filter: '(cn=u1)',
+        scope: 'sub',
+        attributes: ['cn', 'userPassword'],
+        timeLimit: 5
     };
-    qrcode.toDataURL('otpauth://totp/root@localhost.localdomain?secret=AB7JZJXDW4LS3SEZ5OF5J4TDHM&issuer=localhost.localdomain', opts, (err, src) => {
-        if (err) res.send("Error occured");
-        res.send(`<!DOCTYPE html>
-        <html>
-        <head>
-            <title>Sign up</title>
-        </head>
-        <body>
-            <img class="mb-4" src="${src}" alt="" width="200" height="200"/>
-            
-        </body>
-        </html>`);
+    lclient.search('dc=test,dc=com', lopts, (err, ret) => {
+        ret.on('searchRequest', (searchRequest) => {
+            console.log('searchRequest: ', searchRequest.messageID);
+        });
+        ret.on('searchEntry', (entry) => {
+            console.log('entry: ' + JSON.stringify(entry.object));
+        });
+        ret.on('searchReference', (referral) => {
+            console.log('referral: ' + referral.uris.join());
+        });
+        ret.on('error', (err) => {
+            console.error('error: ' + err.message);
+            lclient.unbind();
+        });
+        ret.on('end', (result) => {
+            console.log('status: ' + result);
+            lclient.unbind();
+        });
     });
-})
+    // let opts = {
+    //     margin:10,
+    //     errorCorrectionLevel:'M',
+    //     width: 200
+    // };
+    // qrcode.toDataURL('otpauth://totp/root@localhost.localdomain?secret=AB7JZJXDW4LS3SEZ5OF5J4TDHM&issuer=localhost.localdomain', opts, (err, src) => {
+    //     if (err) res.send("Error occured");
+    //     res.send(`<!DOCTYPE html>
+    //     <html>
+    //     <head>
+    //         <title>Sign up</title>
+    //     </head>
+    //     <body>
+    //         <img class="mb-4" src="${src}" alt="" width="200" height="200"/>
+            
+    //     </body>
+    //     </html>`);
+    // });
+    res.send(`<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Sign up</title>
+    </head>
+    <body>
+        <p>done.</p>
+    </body>
+    </html>`);
+});
